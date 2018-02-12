1b19cfcc26d84956d6f48fd1d1b961d57048d6c5
John Muchovej <ionlights@users.noreply.github.com>
2018-02-12 13:45:47 -0500

    Removed /course link and added Syllabus to homepage

--- assets/scripts/hideme.js
+++ assets/scripts/hideme.js
@@ -1,13 +1,12 @@
 function hideme() {
-  var btns = {};
-  btns.yt = document.getElementsByClassName("btn-youtube");
-  btns.fb = document.getElementsByClassName("btn-facebook");
+    var btns = {};
+    btns.yt = document.getElementsByClassName("btn-youtube");
+    btns.fb = document.getElementsByClassName("btn-facebook");
 
-  for (var yt of btns.yt) {
-      yt.style.visibility = (yt.href === document.URL) ? "hidden" : "visibile";
-  };
-
-  for (var fb of btns.fb) {
-      fb.style.visibility = (fb.href === document.URL) ? "hidden" : "visibile";
-  };
-};
+    for (var yt of btns.yt) {
+        yt.style.visibility = (yt.href === document.URL) ? "hidden" : "visibile";
+    }
+    for (var fb of btns.fb) {
+        fb.style.visibility = (fb.href === document.URL) ? "hidden" : "visibile";
+  }
+}
